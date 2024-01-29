6641695505830e41b05393833486623885541b55
unknown <ppd491@protonmail.com>
2024-01-29 02:29:35 +0100

    first commit, messy code. First objective: implement a cycle-inaccurate CPU.

---
+++ arm/arm7tdmi.cpp
@@ -0,0 +1,198 @@
+#include "arm7tdmi.h"
+#include <windows.h>
+#include "tests.h"
+
+#define IS_BRANCH_AND_EXCHANGE(op)    ((op & 0x0FFFFFF0) == 0x012FFF10)
+#define IS_SOFTWARE_INTERRUPT(op)     ((op & 0x0F000000) == 0x0F000000)
+#define IS_BRANCH(op)                 ((op & 0x0E000000) == 0x0A000000)
+#define IS_BLOCK_DATA_TRANSFER(op)    ((op & 0x0E000000) == 0x08000000)
+#define IS_SINGLE_DATA_TRANSFER(op)    ((op & 0x0E000000) == 0x06000000)
+#define IS_DATA_PROCESSING(op)        ((op & 0x0E000000) == 0x02000000)
+#define IS_HALFWORD_DATA_TRANSFER(op) ((op & 0x0E000000) == 0x000000B0)
+#define IS_SINGLE_DATA_SWAP(op)       ((op & 0x0FB00FF0) == 0x01000090)
+#define IS_MULTIPLY(op)               ((op & 0x0F0000F0) == 0x00000090)
+
+bool evalCondition(u32 cpsr, u32 op) {
+    switch (op & 0xF0000000) {
+    case ARM7TDMI_CONDITION_EQ:
+        return FLAG(Z) == Z;
+    case ARM7TDMI_CONDITION_NE:
+        return FLAG(Z) == 0;
+    case ARM7TDMI_CONDITION_CS:
+        return FLAG(C) == C;
+    case ARM7TDMI_CONDITION_CC:
+        return FLAG(C) == 0;
+    case ARM7TDMI_CONDITION_MI:
+        return FLAG(N) == N;
+    case ARM7TDMI_CONDITION_PL:
+        return FLAG(N) == 0;
+    case ARM7TDMI_CONDITION_VS:
+        return FLAG(V) == V;
+    case ARM7TDMI_CONDITION_VC:
+        return FLAG(V) == 0;
+    case ARM7TDMI_CONDITION_HI:
+        return (FLAG(C) == C) && (FLAG(Z) == 0);
+    case ARM7TDMI_CONDITION_LS:
+        return (FLAG(C) == 0) || (FLAG(Z) == Z);
+    case ARM7TDMI_CONDITION_GE:
+        return (FLAG(N) == FLAG(V));
+    case ARM7TDMI_CONDITION_LT:
+        return (FLAG(N) != FLAG(V));
+    case ARM7TDMI_CONDITION_GT:
+        return (FLAG(Z) == 0) && (FLAG(N) == FLAG(V));
+    case ARM7TDMI_CONDITION_LE:
+        return (FLAG(Z) == Z) || (FLAG(N) != FLAG(V));
+    case ARM7TDMI_CONDITION_AL:
+        return true;
+    case ARM7TDMI_CONDITION_NV:
+    default:
+        return false;
+    }
+}
+
+u32 Arm7tdmi::rReg(u8 reg) {
+    if (reg < 8) {
+        return r[reg];
+    }
+    switch (CURRENT_MODE) {
+        case ARM7TDMI_MODE_USER:
+        case ARM7TDMI_MODE_SYS:
+			return r[reg];
+        case ARM7TDMI_MODE_FIQ:
+            return (reg == 15) ? r[15] + 4 : rFiq[reg - 8];
+        case ARM7TDMI_MODE_SVC:
+            return ((reg == 13) || (reg == 14)) ? rSvc[reg-13] : r[reg];
+        case ARM7TDMI_MODE_ABT:
+			return ((reg == 13) || (reg == 14)) ? rAbt[reg-13] : r[reg];
+        case ARM7TDMI_MODE_IRQ:
+            return ((reg == 13) || (reg == 14)) ? rIrq[reg - 13] : r[reg];
+        case ARM7TDMI_MODE_UND:
+			return ((reg == 13) || (reg == 14)) ? rUnd[reg - 13] : r[reg];
+    }
+    return 0;
+}
+
+void Arm7tdmi::wReg(u8 reg, u32 value) {
+	if (reg < 8) {
+		r[reg] = value;
+	}
+	else {
+		switch (CURRENT_MODE) {
+		case ARM7TDMI_MODE_USER:
+		case ARM7TDMI_MODE_SYS:
+			r[reg];
+		case ARM7TDMI_MODE_FIQ:
+			(reg >= 15) ? r[15] = value : rFiq[reg - 13] = value;
+		case ARM7TDMI_MODE_SVC:
+			((reg == 13) || (reg == 14)) ? rSvc[reg - 13] = value : r[reg] = value;
+		case ARM7TDMI_MODE_ABT:
+			((reg == 13) || (reg == 14)) ? rAbt[reg - 13] = value : r[reg] = value;
+		case ARM7TDMI_MODE_IRQ:
+			((reg == 13) || (reg == 14)) ? rIrq[reg - 13] = value : r[reg] = value;
+		case ARM7TDMI_MODE_UND:
+			((reg == 13) || (reg == 14)) ? rUnd[reg - 13] = value : r[reg] = value;
+		}
+	}
+}
+
+u32 Arm7tdmi::getSPSRValue() {
+	switch (CURRENT_MODE) {
+	    case ARM7TDMI_MODE_FIQ:return spsr[0];
+	    case ARM7TDMI_MODE_IRQ:return spsr[1];
+	    case ARM7TDMI_MODE_SVC:return spsr[2];
+	    case ARM7TDMI_MODE_ABT:return spsr[3];
+	    case ARM7TDMI_MODE_UND:return spsr[4];
+	    default:return cpsr;
+	}
+}
+
+void Arm7tdmi::evaluateArm(u32 op) {
+	if (!evalCondition(cpsr, op)) {
+        return;
+    }
+    if (IS_BRANCH_AND_EXCHANGE(op)) {
+        BX(op);
+    }
+    else if (IS_BLOCK_DATA_TRANSFER(op)) {
+        // Block Data Transfer
+    } 
+    else if (IS_BRANCH(op)) {
+        B_BL(op);
+    }
+    else if (IS_SOFTWARE_INTERRUPT(op)) {
+        // Software Interrupt
+    }
+    else if (IS_SINGLE_DATA_TRANSFER(op)) {
+        // Single Data Transfer
+    }
+	else if (IS_DATA_PROCESSING(op)) {
+		switch (ARM7TDMI_ARM_EXTRACT_OPCODE(op)) {
+		    case 0: AND(op); break;
+		    case 1: EOR(op); break;
+		    case 2: SUB(op); break;
+		    case 3: RSB(op); break;
+		    case 4: ADD(op); break;
+		    case 5: ADC(op); break;
+		    case 6: SBC(op); break;
+		    case 7: RSC(op); break;
+		    case 8: TST(op); break;
+		    case 9: TEQ(op); break;
+		    case 10: CMP(op); break;
+		    case 11: CMN(op); break;
+		    case 12: ORR(op); break;
+		    case 13: MOV(op); break;
+		    case 14: BIC(op); break;
+		    case 15: MVN(op); break;
+		}
+    }
+    else if (IS_HALFWORD_DATA_TRANSFER(op)) {
+        // Halfword Data Transfer ( immediate OR register offset )
+    }
+    else if (IS_SINGLE_DATA_SWAP(op)) {
+        // Single Data Swap
+    }
+    else if (IS_MULTIPLY(op)) {
+        execMultiply(op);
+    }
+    else {
+        // Undefined
+    }
+}
+
+Arm7tdmi::Arm7tdmi(Bus* bus) : bus(bus){}
+
+void Arm7tdmi::printRegsUserMode() {
+    for (int i = 0; i < 16; i++) {
+        printf("r%02d: %08x\n", i, r[i]);
+    }
+    printf("cpsr: %08x\n", cpsr);
+}
+
+int main(){
+    Arm7tdmi cpu = Arm7tdmi(new Bus());
+    cpu.cpsr |= T;
+   
+    while (0) {
+        if (cpu.cpsr & T) { // Thumb mode
+            u16 op = 0;// cpu.read16(r[15]);
+			cpu.evaluateThumb(op);
+			cpu.r[15] += 2;
+		}
+        else { // ARM mode
+            u32 op = 0;// cpu.read32(r[15]);
+            
+                cpu.evaluateArm(op);
+            
+			cpu.r[15] += 4;
+		}
+    }
+
+    //testWrite1(&cpu);
+
+    //test1(&cpu);
+   // cpu.printRegsUserMode();
+    testMOVFlow(&cpu);
+    cpu.printRegsUserMode();
+
+}
+
---
+++ arm/arm7tdmi_thumb.cpp
@@ -0,0 +1,105 @@
+#include "arm7tdmi.h"
+#define REG_53(x) ((x>>3) & 0x7) // register pointed from bits 5 to 3
+#define REG_20(x) (x & 0x7) // register pointed from bits 2 to 0
+#define REG_10_8(x) ((x>>8) & 0x7) // register pointed from bits 10 to 8
+
+
+// THUMB operations
+
+void Arm7tdmi::evaluateThumb(u16 op) {
+	if ((op & 0xFC00) == 0x4000) {
+		// THUMB Alu Operation
+	}
+	else{//  ((op & 0xF000) == 0xD000) { 
+		thumbCondBranch(op);
+	}
+}
+
+void Arm7tdmi :: thumbCondBranch(u16 op) {
+	cpsr = 0;
+}
+
+void Arm7tdmi::TB_MOV1(u16 op) {
+	u8 rd = REG_10_8(op);
+	u8 imm = op & 0xFF;
+	r[rd] = imm;
+	cpsr = (cpsr & ~N) | ((imm & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((imm == 0) ? Z : 0);
+}
+
+
+void Arm7tdmi::TB_ROR(u16 op) {
+	u8 rsVal = r[REG_53(op)];
+	u8 rd = REG_20(op);
+	u32 rdVal = r[rd];
+	u8 rs4to0 = rsVal & 0x1F; // Rs[4:0]
+	if (rsVal == 0) {
+
+	}
+	else if (rs4to0 == 0) {
+		cpsr = (cpsr & ~C) | ((rdVal & BIT(31)) ? C : 0);
+	}
+	else {
+		cpsr = (cpsr & ~C) | ((rdVal & BIT(rs4to0 - 1)) ? C : 0);
+		rdVal = (rdVal >> rs4to0) | (rdVal << (32 - rs4to0));
+		r[rd] = rdVal;
+		cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+		cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+	}
+}
+
+void Arm7tdmi::TB_AND(u16 op) {
+	u8 rd = REG_20(op);
+	u8 rm = REG_53(op);
+	r[rd] = r[rd] & r[rm];
+	cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+}
+
+/**
+Rd = Rd EOR Rm
+N Flag = Rd[31]
+Z Flag = if Rd == 0 then 1 else 0
+*/
+void Arm7tdmi::TB_EOR(u16 op) {
+	u8 rd = REG_20(op);
+	u8 rm = REG_53(op);
+	r[rd] = r[rd] ^ r[rm];
+	cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+}
+
+void Arm7tdmi::TB_ORR(u16 op) {
+	u8 rd = REG_20(op);
+	u8 rm = REG_53(op);
+	r[rd] = r[rd] | r[rm];
+	cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+}
+
+//Rd = Rd AND NOT Rm
+//N Flag = Rd[31]
+//Z Flag = if Rd == 0 then 1 else 0
+void Arm7tdmi::TB_BIC(u16 op) {
+	u8 rd = REG_20(op);
+	u8 rm = REG_53(op);
+	r[rd] = r[rd] & ~r[rm];
+	cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+}
+
+void Arm7tdmi::TB_MVN(u16 op) {
+	u8 rd = REG_20(op);
+	u8 rm = REG_53(op);
+	r[rd] = ~r[rm];
+	cpsr = (cpsr & ~N) | ((r[rd] & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((r[rd] == 0) ? Z : 0);
+}
+
+void Arm7tdmi::TB_TST(u16 op) {
+	u8 rn = REG_20(op);
+	u8 rm = REG_53(op);
+	u8 alu_out = r[rn] & r[rm];
+	cpsr = (cpsr & ~N) | ((alu_out & BIT(31)) ? N : 0);
+	cpsr = (cpsr & ~Z) | ((alu_out == 0) ? Z : 0);
+}
